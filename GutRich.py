da850954c2dfbcce66ebb72e613db8360e5df697
mamchurd <69331465+mamchurd@users.noreply.github.com>
2022-04-15 11:46:55 +1100

    Update GutRich.py

--- GutRich.py
+++ GutRich.py
@@ -1,5 +1,19 @@
-import pandas as pan
+import os
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-data = pan.read_excel("reports_list_12-25-20012021.xlsx")
-print(type(data.mag))
+cwd = os.getcwd();
+file = "reports_list_12-25-20012021.xlsx"
+xl = pd.ExcelFile(file)
+df = xl.parse('Reports list')
+magArray = df.mag.values
+вф
+maxMag = max(magArray)
+minMag = min(magArray)
+steps = int((maxMag - minMag) * 10 + 1)
+linspace = np.linspace(minMag, maxMag, steps)
+hist = np.histogram(magArray, steps)[0]
+linspace = linspace[::-1]
+hist = hist[::-1]
+cumsum = np.cumsum(hist)
